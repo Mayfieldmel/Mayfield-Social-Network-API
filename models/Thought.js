20d51fd1d744c069324beed62d733693d7bdc9d9
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-09-30 15:45:19 -0400

    create reaction schema & subdocument

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,36 @@
-const {Schema, Type, model} = require('mongoose');
+const {Schema, Types, model} = require('mongoose');
 const dateFormat = require("../utils/dateFormat");
+
+
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        maxLength: 280,
+        required: [true, "Reaction text is required"],
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    }
+},
+{
+    toJSON: {
+        getters: true
+    },
+    id: false 
+}
+)
 
 const thoughtSchema = new Schema({
     thoughtText: {
