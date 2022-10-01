128b789fd5fc9cd7700b942c456835049da6b699
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-01 13:52:33 -0400

    contoller for creat & update user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,18 +1,18 @@
-const { User } = require('../models');
+const { User } = require("../models");
 
 const userController = {
   // get all users
   getAllUsers(req, res) {
     User.find({})
       .populate({
-        path: 'Thoughts',
-        select: '-__v'
+        path: "Thoughts",
+        select: "-__v",
       })
       .populate({
-        path: 'Users',
-        select: '-__v'
+        path: "Users",
+        select: "-__v",
       })
-      .select('-__v')
+      .select("-__v")
       .sort({ _id: -1 })
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => {
@@ -24,26 +24,51 @@ const userController = {
   // get user by id
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
-    .populate({
-        path: 'Thoughts',
-        select: '-__v'
-    })
-    .populate({
-        path: 'Users',
-        select: '-__v'
-    })
-    .select('-__v')
-    .then(dbUserData => {
+      .populate({
+        path: "Thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "Users",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
         // if no user is found, send 404
-        if(!dbUserData) {
-            res.status(404).json({message: 'No user found with this id!'});
-            return;
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
         }
         res.json(dbUserData);
-    })
-    .catch(err => {
+      })
+      .catch((err) => {
         console.log(err);
         res.status(400).json(err);
-    })
+      });
   },
+
+  // create new user
+  createUser({body}, res) {
+    User.create(body)
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => res.status(400).json(err))
+  },
+
+    // update User
+  updateUser({body, params}, res) {
+    User.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+    .then(dbUserData => {
+       // if no user is found, send 404
+       if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id!" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    }); 
+  }
 };
+
