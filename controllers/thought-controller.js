6580cf949f7bec59e3914f453bda50efc58acbd1
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-01 15:03:58 -0400

    controller for add thought/update user

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -4,11 +4,30 @@ const thoughtController = {
   // get all thoughts
   getAllThoughts(req, res) {
     Thought.find({})
-        // populate?
-        .select('-__v')
-        .sort({ createdAt: -1 })
-        .then(dbThoughtData => res.json(dbThoughtData))
-      .catch(err => {
+      // populate reactions?
+      .select("-__v")
+      .sort({ createdAt: -1 })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // get one thought by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      // populate reactions?
+      .select("-__v")
+      .then((dbThoughtData) => {
+        // If no thought is found, send 404
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
         console.log(err);
         res.status(400).json(err);
       });
@@ -16,6 +35,17 @@ const thoughtController = {
 
   // add thought
   addThought({ params, body }, res) {
-    Thought.create
+    Thought.create(body)
+      // expects: {"thoughtText": "Here's a cool thought...", "username": "lernantino", "userId": "5edff358a0fcb779aa7b118b"}
+      // destructure body?
+      .then((dbThoughtData) => {
+        User.findOneAndUpdate(
+          { _id: dbThoughtData.userId },
+          { $push: { thoughts: dbThoughtData._id } },
+          { new: true, runValidators: true }
+        );
+      })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.status(400).json(err));
   },
 };
