1f081a7a980557737d3b2a0a8b28d56b85781faa
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-01 14:11:02 -0400

    controller for add friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,5 @@
 const { User } = require("../models");
+const { findOne, findOneAndUpdate } = require("../models/User");
 
 const userController = {
   // get all users
@@ -49,6 +50,7 @@ const userController = {
 
   // create new user
   createUser({body}, res) {
+    // expects: { "username": "lernantino", "email": "lernantino@gmail.com" }
     User.create(body)
     .then(dbUserData => res.json(dbUserData))
     .catch(err => res.status(400).json(err))
@@ -73,6 +75,23 @@ const userController = {
 //   delete user
   deleteUser({params}, res) {
     User.findOneAndDelete({_id: params.id})
+    .then(dbUserData => {
+        // if no user is found, send 404
+        if (!dbUserData) {
+         res.status(404).json({ message: "No user found with this id!" });
+         return;
+       }
+       res.json(dbUserData);
+     })
+     .catch((err) => {
+       console.log(err);
+       res.status(400).json(err);
+     }); 
+  },
+
+//   add friend to user's friend list
+  addFriend({params}, res) {
+    findOneAndUpdate({_id: params.userId}, {$push: {friends: params.friendId}}, {new: true})
     .then(dbUserData => {
         // if no user is found, send 404
         if (!dbUserData) {
