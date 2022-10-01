46b8431d04b854bcef4854995371e71c7b21dfc4
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-01 13:35:04 -0400

    controller for get one user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,18 +1,18 @@
-const { User } = require("../models");
+const { User } = require('../models');
 
 const userController = {
   // get all users
   getAllUsers(req, res) {
     User.find({})
       .populate({
-        path: "Thoughts",
-        select: "-__v",
+        path: 'Thoughts',
+        select: '-__v'
       })
       .populate({
-        path: "Users",
-        select: "-__v",
+        path: 'Users',
+        select: '-__v'
       })
-      .select("-__v")
+      .select('-__v')
       .sort({ _id: -1 })
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => {
@@ -23,6 +23,27 @@ const userController = {
 
   // get user by id
   getUserById({ params }, res) {
-    User.findOne({ _id: params.id });
+    User.findOne({ _id: params.id })
+    .populate({
+        path: 'Thoughts',
+        select: '-__v'
+    })
+    .populate({
+        path: 'Users',
+        select: '-__v'
+    })
+    .select('-__v')
+    .then(dbUserData => {
+        // if no user is found, send 404
+        if(!dbUserData) {
+            res.status(404).json({message: 'No user found with this id!'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
   },
 };
