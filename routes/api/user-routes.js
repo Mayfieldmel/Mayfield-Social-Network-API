fe85f6bbbbed9576e37704ce6ba65faea2251f7f
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-02 12:10:01 -0400

    add user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -9,4 +9,23 @@ const {
   removeFriend,
 } = require("../../controllers/user-controller");
 
-// 
+// GET all & POST at /api/users
+router
+    .route("/")
+    .get(getAllUsers)
+    .post(createUser)
+
+// GET, PUT, & DELETE one at /api/users/id
+router
+    .route("/:id")
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+// PUT(add) & DELETE friend at /api/users/userId/friends/friendId
+router
+    .route("/:userId/friends/:friendId")
+    .put(addFriend)
+    .delete(removeFriend)
+
+module.exports = router;
